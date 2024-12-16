96697d2befd60cd07e8bec61a4bf36c68ca8e5ae
Aiana <ayanaibraiimkyzy@gmail.com>
2024-12-16 15:40:35 +0100

    commit

--- sw.js
+++ sw.js
@@ -1,53 +1,140 @@
-// This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
+const cacheName = 'travelboss-cache-v1';
 
-const CACHE = "pwabuilder-offline-page";
-
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "index.html";
-
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
+//Install SW
+self.addEventListener('install', (event) => {
+    console.log('ServiceWorker installed')
 });
 
-self.addEventListener('install', async (event) => {
-  event.waitUntil(
-    caches.open(CACHE)
-      .then((cache) => cache.add(offlineFallbackPage))
-  );
-});
+self.addEventListener('activate', (event) => {
+    console.log('ServiceWorker activated');
 
-if (workbox.navigationPreload.isSupported()) {
-  workbox.navigationPreload.enable();
-}
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.filter((name) => name !== cacheName).map((name) =>caches.delete(name))        
+        )
+    })
+)
+})
 
-workbox.routing.registerRoute(
-  new RegExp('/*'),
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: CACHE
-  })
-);
+//Network First
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request)
+        .then((response) => {
+            return caches.open(cacheName).then((cache) => {
+                cache.put(event.request, response.clone());
+                return response;
+            })
+        }).catch(() => {
+            return caches.match(event.request)
+        })
+    )
+})
+
+// 1. Cache First (cache falling back to network)
 
 self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        const preloadResp = await event.preloadResponse;
-        if (preloadResp) {
-          return preloadResp;
-        }
+    event.respondWith(
+        caches.match(event.request)
+        .then((cachedResponse) => {
+            if (cachedResponse) {
+                return cachedResponse;
+            }
 
-        const networkResp = await fetch(event.request);
-        return networkResp;
-      } catch (error) {
+            return fetch(event.request)
+            .then((networkResponse) => {
+                return caches.open(cacheName)
+                .then((cache) => {
+                    cache.put(event.request, networkResponse.clone());
+                    return networkResponse;
+                });
+            });
+        })
+    );
+});
 
-        const cache = await caches.open(CACHE);
-        const cachedResp = await cache.match(offlineFallbackPage);
-        return cachedResp;
-      }
-    })());
-  }
+//2.Network First
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request)
+        .then((response) => {
+            return caches.open(cacheName).then((cache) => {
+                cache.put(event.request, response.clone());
+                return response;
+            });
+        })
+        .catch(() => {
+            return caches.match(event.request);
+        })
+    );
+});
+
+// 3.Cache only
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+    );
+});
+
+//4.Network only
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request)
+    );
+});
+
+//5. Stale while revalidate
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.open(cacheName).then((cache) => {
+            return cache.match(event.request).then((cachedResponse) => {
+                const fetchPromise = fetch(event.request).then((networkResponse) => {
+                    cache.put(event.request, networkResponse.clone());
+                    return networkResponse;
+                });
+                return cachedResponse || fetchPromise;
+            });
+        })
+    );
+});
+
+//6.Offline Fallback
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            return caches.match('/offline.html');
+        })
+    );
+});
+
+//7.Custom Strategy
+
+self.addEventListener('fetch', (event) => {
+    const url = new URL(event.request.url);
+    if (url.origin === location.origin) {
+        event.respondWith(
+            caches.match(event.request).then((response) => {
+                return response || fetch(event.request);
+            })
+        );
+    } else {
+        event.respondWith(
+            fetch(event.request)
+            .then((response) => {
+                return caches.open(cacheName).then((cache) => {
+                    cache.put(event.request, response.clone());
+                    return response;
+                });
+            })
+            .catch(() => {
+                return caches.match(event.request);
+            })
+        );
+    }
 });
